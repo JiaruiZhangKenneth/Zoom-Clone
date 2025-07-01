469fc0f38942c4bd74c845b3e5cc7b81b4f52ff1
zjrken <zjrken@umich.edu>
2025-07-01 12:11:19 -0400

    https

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const fs = require("fs");
+const https = require("https");
 const { v4: uuidV4 } = require("uuid");
+const { Server } = require("socket.io");
+
+const app = express();
+
+// Load SSL cert and key
+const server = https.createServer(
+    {
+        key: fs.readFileSync("/home/ubuntu/ssl/privkey.pem"),
+        cert: fs.readFileSync("/home/ubuntu/ssl/cert.pem"),
+    },
+    app
+    );
+
+// Initialize Socket.IO
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+    app.get("/", (req, res) => {
     res.redirect(`/${uuidV4()}`);
-});
+    });
 
-app.get("/:room", (req, res) => {
+    app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
-});
+    });
 
-io.on("connection", (socket) => {
-socket.on("join-room", (roomId, userId) => {
-socket.join(roomId);
-socket.to(roomId).emit("user-connected", userId);  // Notify to all other users in the room that a new user has connected
+    io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("user-connected", userId); // Notify to all other users in the room that a new user has connected
 
-socket.on("disconnect", () => {
-    socket.to(roomId).emit("user-disconnected", userId);
-});
-});
-});
+        socket.on("disconnect", () => {
+        socket.to(roomId).emit("user-disconnected", userId);
+        });
+    });
+    });
 
 server.listen(3030);
